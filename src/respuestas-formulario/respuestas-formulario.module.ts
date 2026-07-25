d1325a5d252c7677dbc3eaf624476e536db00d93
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-25 15:26:24 -0500

    Update respuestas-formulario.module.ts

--- src/respuestas-formulario/respuestas-formulario.module.ts
+++ src/respuestas-formulario/respuestas-formulario.module.ts
@@ -4,9 +4,13 @@ import { RespuestasFormulario } from './entities/respuestas-formulario.entity';
 import { RespuestaOpcionSeleccionada } from './entities/respuestas-opciones-seleccionadas.entity'; 
 import { RespuestasFormularioController } from './respuestas-formulario.controller';
 import { RespuestasFormularioService } from './respuestas-formulario.service';
+import { DocumentosRespaldoModule } from 'src/documentos-respaldo/documentos-respaldo.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RespuestasFormulario, RespuestaOpcionSeleccionada])], 
+  imports: [TypeOrmModule.forFeature([RespuestasFormulario, RespuestaOpcionSeleccionada]),
+  DocumentosRespaldoModule,
+],
+
   controllers: [RespuestasFormularioController],
   providers: [RespuestasFormularioService],
   exports: [TypeOrmModule, RespuestasFormularioService],
