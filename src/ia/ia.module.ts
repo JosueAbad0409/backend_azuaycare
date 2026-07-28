0c665ef94b8c80d4562cff6399d65c1da8d9b32b
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-28 10:55:53 -0500

    IA

--- src/ia/ia.module.ts
+++ src/ia/ia.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { IaService } from './ia.service';
 import { IaController } from './ia.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule
+  ],
   controllers: [IaController],
   providers: [IaService],
 })
