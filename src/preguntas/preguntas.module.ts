9ecbd2bd94fd2946d0f7f4a90c0de9561e0fb28f
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-28 09:30:18 -0500

    update preguntas.module

--- src/preguntas/preguntas.module.ts
+++ src/preguntas/preguntas.module.ts
@@ -5,9 +5,10 @@ import { Seccion } from '../secciones/entities/secciones.entity';
 import { TipoCampoForm } from '../tipos-campo-form/entities/tipos-campo-form.entity';
 import { PreguntasController } from './preguntas.controller';
 import { PreguntasService } from './preguntas.service';
+import { Formulario } from 'src/formularios/entities/formulario.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pregunta, Seccion, TipoCampoForm])],
+  imports: [TypeOrmModule.forFeature([Pregunta, Seccion, TipoCampoForm, Formulario])],
   controllers: [PreguntasController],
   providers: [PreguntasService],
   exports: [TypeOrmModule, PreguntasService],
--- src/secciones/secciones.module.ts
+++ src/secciones/secciones.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Seccion } from './entities/secciones.entity'; // 👈 Ruta adaptada al plural
+import { Seccion } from './entities/secciones.entity'; 
 import { Formulario } from '../formularios/entities/formulario.entity';
 import { SeccionesController } from './secciones.controller';
 import { SeccionesService } from './secciones.service';
