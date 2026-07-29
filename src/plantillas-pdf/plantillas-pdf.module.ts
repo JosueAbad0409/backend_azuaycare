3275a76e8591439cc176537949306f468ea7f6cc
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-07-29 01:31:39 -0500

    module de plantillas y rangos

--- src/plantillas-pdf/plantillas-pdf.module.ts
+++ src/plantillas-pdf/plantillas-pdf.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PlantillasPdfService } from './plantillas-pdf.service';
 import { PlantillasPdfController } from './plantillas-pdf.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlantillaPdf } from './entities/plantillas-pdf.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PlantillaPdf])],
   controllers: [PlantillasPdfController],
   providers: [PlantillasPdfService],
 })
--- src/rangos-variable-calculada/rangos-variable-calculada.module.ts
+++ src/rangos-variable-calculada/rangos-variable-calculada.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RangosVariableCalculadaService } from './rangos-variable-calculada.service';
 import { RangosVariableCalculadaController } from './rangos-variable-calculada.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RangoVariableCalculada } from './entities/rangos-variable-calculada.entity';
+import { Formulario } from 'src/formularios/entities/formulario.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RangoVariableCalculada,Formulario])],
   controllers: [RangosVariableCalculadaController],
   providers: [RangosVariableCalculadaService],
 })
