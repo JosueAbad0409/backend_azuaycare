587399ccd4342495fb92d4b75d70a9ac7b2f9b87
Steven72638 <lunasteven282@gmail.com>
2026-07-30 23:51:07 -0500

    cambio en matrices

--- src/matrices-form/matrices-form.module.ts
+++ src/matrices-form/matrices-form.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FilaMatriz } from './entities/fila-matriz.entity';
 import { ColumnaMatriz } from './entities/columna-matriz.entity';
+import { Pregunta } from '../preguntas/entities/pregunta.entity'; // 👈 Agregar
+import { Seccion } from '../secciones/entities/secciones.entity';   // 👈 Agregar
+import { Formulario } from '../formularios/entities/formulario.entity'; // 👈 Agregar
 import { MatricesFormService } from './matrices-form.service';
 import { MatricesFormController } from './matrices-form.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FilaMatriz, ColumnaMatriz])],
+  imports: [
+    TypeOrmModule.forFeature([
+      FilaMatriz,
+      ColumnaMatriz,
+      Pregunta,   // 👈
+      Seccion,    // 👈
+      Formulario, // 👈
+    ]),
+  ],
   controllers: [MatricesFormController],
   providers: [MatricesFormService],
   exports: [TypeOrmModule, MatricesFormService],
