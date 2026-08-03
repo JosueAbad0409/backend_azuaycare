4644251280e6943556e54250a683a630b223ca43
Josue Abad <josue.abad.est@tecazuay.edu.ec>
2026-08-02 19:51:55 -0500

    Add CoordinadoresCarrera entity to TypeOrmModule for feature integration

--- src/fichas-respondidas/fichas-respondidas.module.ts
+++ src/fichas-respondidas/fichas-respondidas.module.ts
@@ -8,10 +8,11 @@ import { NivelesEconomicosModule } from '../niveles-economicos/niveles-economico
 import { FichaRespuestasListener } from './listeners/ficha-respuestas.listener'; 
 import { MailModule } from 'src/mail/mail.module';
 import { PdfModule } from '../common/pdf/pdf.module'; 
+import { CoordinadoresCarrera } from 'src/coordinadores-carreras/entities/coordinadores-carrera.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([FichaRespondida]),
+    TypeOrmModule.forFeature([FichaRespondida, CoordinadoresCarrera]),
     CacheModule.register({
       ttl: 43200000, // 12 horas de retención en RAM (en milisegundos)
       max: 100,      // Máximo 100 estructuras de formularios en memoria a la vez
